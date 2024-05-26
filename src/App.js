a9e3c8a075ab7ba10ffd497a114a528d24414fce
jamieecode <jamieecode@gmail.com>
2024-05-26 16:22:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+노asfasf놉        </a>
       </header>
     </div>
   );
